0512e4de08369b982c862ba8a550d9cbd2e1d7be
Ross Edlin <rossedlin@gmail.com>
2020-05-12 16:04:08 +0700

    update laravel to bootstrap 4.4 and improve build scripts87

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
+#
+# Composer
+#
 sudo rm -R vendor
-docker run -v $PWD:/var/www -vstripe_demo_composer:/root/.composer rossedlin/centos-apache-php:7.1 composer install
+docker run -v $PWD:/var/www -vstripe_demo_composer:/root/.composer rossedlin/centos-apache-php:7.1-dev composer install
+
+#
+# NPM
+#
+sudo rm -R node_modules
+docker run -v $PWD:/var/www rossedlin/centos-apache-php:7.1-dev npm install
+docker run -v $PWD:/var/www rossedlin/centos-apache-php:7.1-dev npm run dev
--- up.sh
+++ up.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker-compose down
-docker pull rossedlin/centos-apache-php:7.1
+docker pull rossedlin/centos-apache-php:7.1-dev
 docker-compose up
